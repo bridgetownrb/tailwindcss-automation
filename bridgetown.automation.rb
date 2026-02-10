49c4105b06add2e067c5b6cb60b05b76c7816bed
Jarrett Lusso <jclusso@gmail.com>
2026-02-10 01:15:24 -0500

    Improve Tailwind CSS automation setup and configuration to work seemlessly.
    It no longer relies on the JIT builder which caused multiple reloads, destroyed fast refreshing, etc
    
    Additionally, if people want to use the default CSS with this, they can without issues.

--- bridgetown.automation.rb
+++ bridgetown.automation.rb
@@ -2,86 +2,43 @@
 
 say_status :tailwind, "Installing Tailwind CSS..."
 
-confirm = ask "This configuration will ovewrite your existing #{"postcss.config.js".bold.white}. Would you like to continue? [Yn]"
-return unless confirm.casecmp?("Y")
-
-run "npm install tailwindcss @tailwindcss/postcss --save-dev"
-
-create_file "postcss.config.js", <<~JS, force: true
-  export default {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      'postcss-flexbugs-fixes': {},
-      'postcss-preset-env': {
-        autoprefixer: {
-          flexbox: 'no-2009'
-        },
-        stage: 3
-      }
-    }
-  }
-JS
+run "npm install tailwindcss @tailwindcss/cli --save-dev"
 
 css_imports = <<~CSS
-  /* If you need to add @import statements, do so up here */
-
-  @import "jit-refresh.css"; /* triggers frontend rebuilds */
-
-  /* Set up Tailwind imports */
-  @import "tailwindcss";
-
+  @import 'tailwindcss' source(none);
+  @source '../';
+  @source '../../config';
+  @source '../../plugins';
+  @source '../../src';
 CSS
 
-if File.exist?("frontend/styles/index.css")
-  prepend_to_file "frontend/styles/index.css", css_imports
-else
-  say "\nPlease add the following lines to your CSS index file:"
-  say css_imports
-end
-
-create_file "frontend/styles/jit-refresh.css", "/* #{Time.now.to_i} */"
+create_file "frontend/styles/tailwind.css", css_imports
 
 insert_into_file "Rakefile",
-                  after: %r{  task :(build|dev) do\n} do
-  <<-JS
-    sh "touch frontend/styles/jit-refresh.css"
-  JS
+                  after: %r{  task :(build) do\n} do
+  <<-RUBY
+    sh "bin/tailwindcss"
+  RUBY
 end
 
-if File.exist?(".gitignore")
-  append_to_file ".gitignore" do
-    <<~FILES
-
-      frontend/styles/jit-refresh.css
-    FILES
-  end
-end
-
-create_builder "tailwind_jit.rb" do
-  <<~'RUBY'
-    class Builders::TailwindJit < SiteBuilder
-      def build
-        return if ARGV.include?("--skip-tw-jit")
-
-        fast_refreshing = false
-
-        hook :site, :fast_refresh do
-          fast_refreshing = true
-        end
-
-        hook :site, :post_write do
-          if fast_refreshing
-            fast_refreshing = false
-            Thread.new do
-              sleep 0.75
-              refresh_file = site.in_root_dir("frontend", "styles", "jit-refresh.css")
-              File.write refresh_file, "/* #{Time.now.to_i} */"
-            end
-          end
-        end
-      end
+append_to_file "Rakefile" do
+  <<~RUBY
+    Rake::Task['frontend:watcher'].enhance do
+      Bridgetown::Utils::Aux.run_process(
+        'Tailwind',
+        :blue,
+        'bin/tailwindcss --watch'
+      )
     end
   RUBY
 end
 
+get "#{__dir__}/tailwindcss", "bin/tailwindcss"
+chmod "bin/tailwindcss", 0755
+
+gsub_file 'config/esbuild.defaults.js', /const manifest = {/ do |match|
+  match << ' "styles/tailwind.css":"tailwind.css" '
+end
+
 say_status :tailwind, "Tailwind CSS is now configured."
+say_status :tailwind, 'Use `asset_path "styles/tailwind.css"` in your templates to load the CSS file.'
